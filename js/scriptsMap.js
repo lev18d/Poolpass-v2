7d6cdf7ca1d1be0f68acc15a2496e12235c15c89
Tomek <t.limsiewicz@gmail.com>
2016-06-20 12:17:57 +0300

    added multiple marks to the map

--- js/scriptsMap.js
+++ js/scriptsMap.js
@@ -1,16 +1,14 @@
 
 $(document).ready(function(){/* google maps -----------------------------------------------------*/
 
-function initialize() {
-
-
-
     var locations = [
         ['Dan\'s pool' , 32.070278, 34.794167, 4],
         ['Ela\'s pool', 32.091859, 34.793126, 3],
         ['Dror\'s pool', 32.070011, 34.800012, 2],
         ['Eldad\'s pool', 32.045018, 34.760854, 1]
     ];
+
+function initialize() {
 
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 13,
@@ -31,14 +29,20 @@ function initialize() {
         });
         google.maps.event.addListener(marker, 'click', (function(marker, i) {
             return function() {
-                infowindow.setContent(locations[i][0]);
+                infowindow.setContent('<p> '+locations[i][0]+'</p>'+'<a href="http://www.google.com"<button>Visit</button>');
                 infowindow.open(map, marker);
             }
         })(marker, i));
 
 
     }
-};
+
+
+}
     google.maps.event.addDomListener(window, 'load', initialize);
 /* end google maps -----------------------------------------------------*/
+
+
+
+
 });
